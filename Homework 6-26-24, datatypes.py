61255e1d26f9ff7b6bb3f917fda55a8df9c979ed
Sataticherla <sameer.taticherla@gmail.com>
2024-07-07 12:11:36 -0700

    Create Homework 6-26-24, datatypes.py

---
+++ Homework 6-26-24, datatypes.py
@@ -0,0 +1,21 @@
+# Case sensitive are things that require to be all lowercase. An example of this is print() is not the same as Print().
+
+# Case insensitive is are things that don't matter if they are upper nor lowercase, such as variable. "x" coulde be a variable as well as "X". Both could work
+
+
+# A string is text
+# A integer is a positive and non-decimal number
+# A float is a decimal number (2.0 still counts as a float)
+# A boolean is a true or false statement
+# A list is a list of things
+# A tuple is a list of things that cannot be changed or added with further things
+# A dictionary is a list of things that are paired with other things
+# A variable is a item that stores a value
+
+Tuple_kinda_sounds_like_a_flower_dont_you_think = ("First homework-Complete", 123, 3.141592653589793238, True, [4, 5, 6], (7, 8, 9), {"a": 10, "b": 11, "c": 12},)
+baloney ="(*/ω＼*)....." #<<<----- thats my variable I couldn't put it in the tuple (ಥ_ಥ)
+
+print(Tuple_kinda_sounds_like_a_flower_dont_you_think)
+print("")
+print(baloney)
+
